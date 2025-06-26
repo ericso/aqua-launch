ecf10ac78ee435da446d8f38b57a0b66b03ad508
Eric So <me@eric.so>
2025-06-26 11:38:55 -0700

    tweak physics impulse numbers, re-work spawn rate to be dependent on on-screen rings

--- main.gd
+++ main.gd
@@ -5,14 +5,36 @@ extends Node2D
 
 var spawn_timer := 0.0
 
-@export var spawn_interval := 1.5 # seconds
+# spawn rate of rings is dependent on the number of rings on screen
+# more rings on screen increases the spawn interval
+@export var spawn_interval := 1.0 # default spawn rate in seconds
+@export var base_interval := 1.0
+@export var interval_per_ring := 3.0
+@export var max_interval := 10.0
+
+@export var nudge_force = 100000
+var min_nudge_force = 0
+var max_nudge_force = nudge_force / 2
 
 func _process(delta: float) -> void:
 	spawn_timer += delta
-	if spawn_timer >= spawn_interval:
-		spawn_timer = 0;
-		spawn_ring()
+	
+	var visible_ring_count = get_visible_ring_count()
+	spawn_interval = clamp(base_interval + (visible_ring_count * interval_per_ring), base_interval, max_interval)
 
+	if spawn_timer >= spawn_interval:
+		spawn_timer = 0
+		spawn_ring()
+		
+func get_visible_ring_count() -> int:
+	var count := 0
+	var viewport_rect := get_viewport().get_visible_rect()
+
+	for ring in get_tree().get_nodes_in_group("rings"):
+		if ring is Node2D and viewport_rect.has_point(ring.global_position):
+			count += 1
+	return count
+	
 func spawn_ring():
 	var ring = ring_scene.instantiate()
 	var x_pos = randf_range(50, 670) # Avoid edges of 720px wide screen
@@ -36,5 +58,5 @@ func apply_wave_impulse(tap_pos: Vector2) -> void:
 			var distance = dir.length()
 			if distance == 0:
 				continue
-			var force = clamp(5000 / distance, 50, 600)  # Inverse falloff
+			var force = clamp(nudge_force / distance, min_nudge_force, max_nudge_force)  # Inverse falloff
 			ring.apply_impulse(dir.normalized() * force)
--- ring.gd
+++ ring.gd
@@ -1,11 +1,12 @@
 extends RigidBody2D
 
-@export var floating_damp := 6.5 # damping to simulate water resistance
+@export var floating_damp := 4 # damping to simulate water resistance
 
 func _process(delta) -> void:
 	if position.y > 1400:
 		queue_free()
 
 func _ready():
+	gravity_scale = 0.6
 	linear_damp = floating_damp
 	angular_damp = floating_damp
